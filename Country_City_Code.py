5067da8dbfc0a726990ef699475357bbb0a6295b
AkshmitDewan <97413756+AkshmitDewan@users.noreply.github.com>
2023-06-05 15:25:14 +0530

    Update Country_City_Code.py

--- Country_City_Code.py
+++ Country_City_Code.py
@@ -1,24 +1,31 @@
 import json
-def get_country_city_code(country,city,docu):
-    country_code=0
-    city_code=0
+
+def get_country_city_code(country, city, docu):
+    country_code = 0
+    city_code = 0
+    doc_code = 0
+
     with open('countries.json') as json_file:
         data = json.load(json_file)
         try:
-            country_code=data[country]
-        except:
-            country_code=101
+            country_code = data[country]
+        except KeyError:
+            country_code = 101
+
     with open('cities.json') as cities:
-        data=json.load(cities)
+        data = json.load(cities)
         try:
-            city_code=data[country][city]
-        except:
+            city_code = data[country][city]
+        except KeyError:
             city_code = 12
-    with open('doc_type.json'):
-        data=json.load(docu)
+
+    with open('doc_type.json') as doc_type:
+        data = json.load(doc_type)
         try:
-            doc_code=data[docu]
-        except:
+            doc_code = data[docu]
+        except KeyError:
             doc_code = 1
-    return country_code,city_code,doc_code
+
+    return country_code, city_code, doc_code
+
     
